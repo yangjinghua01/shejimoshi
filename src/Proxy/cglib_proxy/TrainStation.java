c672cf44439521b769bc939f0a7c213e000cdf31
16920 <1692047690@qq.com>
2022-02-28 21:25:10 +0800

    CGLIB动态代理

---
+++ src/Proxy/cglib_proxy/TrainStation.java
@@ -0,0 +1,11 @@
+package Proxy.cglib_proxy;
+/**
+ * @Title CGLIB代理的火车类
+ * @Time 2022年2月21日 11:44:34
+ * @Author：yjh
+ */
+public class TrainStation  {
+    public void sell() {
+        System.out.println("火车站卖票");
+    }
+}
